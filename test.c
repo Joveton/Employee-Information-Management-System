dad1ce2a57d72e53b05756dfeea20e4df2cf697c
wangpeng007-coder <2016191816@qq.com>
2019-09-23 23:08:05 -0400

    add test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: wangpeng 
+	#	 Email		: wp_21859@163.com 
+	#	 Created	: Mon 23 Sep 2019 11:07:45 PM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}
